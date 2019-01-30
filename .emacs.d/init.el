b2da6634c855267d3e52d7deb2d96ea1e2c4d780
zettaittenani <zettaittenani@gmail.com>
2019-01-30 18:36:44 +0900

    Revise

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -281,7 +281,6 @@
 (add-hook 'rust-mode-hook 'racer-mode)
 (add-hook 'racer-mode-hook 'eldoc-mode)
 (add-hook 'racer-mode-hook 'company-mode)
-
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
 
@@ -298,10 +297,8 @@
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("[Rr]akefile$" . ruby-mode))
-
 (use-package auto-highlight-symbol)
 (global-auto-highlight-symbol-mode t)
-
 (autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
 (autoload 'robe-ac-setup "robe-ac" "robe auto-complete" nil nil)
 (add-hook 'robe-mode-hook 'robe-ac-setup)
