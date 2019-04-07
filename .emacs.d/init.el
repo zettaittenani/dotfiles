48d354b5dad750d3c508909b135e1aad3c109dd1
zettaittenani <zettaittenani@gmail.com>
2019-04-07 18:38:46 +0900

    コメントアウトを外したとき toggle-fullscreen が効かないので修正する

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -298,17 +298,17 @@
 ;; if you use GUI
 ;; (defun my-fullscreen ()
 ;;   (interactive)
-;;   (set-frame-parameter nil 'fullscreen 'fullboth)
-;;
+;;   (set-frame-parameter nil 'fullscreen 'fullboth))
+
 ;; (defun my-non-fullscreen ()
 ;;   (interactive)
 ;;   (set-frame-parameter nil 'width 82)
-;;   (set-frame-parameter nil 'fullscreen 'fullheight)
-;;
+;;   (set-frame-parameter nil 'fullscreen 'fullheight))
+
 ;; (defun toggle-fullscreen ()
 ;;   (interactive)
 ;;   (if (eq (frame-parameter nil 'fullscreen) 'fullboth)
 ;;       (my-non-fullscreen)
 ;;     (my-fullscreen)))
-;;
+
 ;; (global-set-key (kbd "<f2>") 'toggle-fullscreen)
