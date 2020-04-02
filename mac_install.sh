b7ed80da8d9e6ee8dd7fa9c46e7b4f1bc49c6bb5
zettaittenani <zettaittenani@gmail.com>
2020-04-02 14:55:57 +0900

    Create setup script for Mac

---
+++ mac_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+# Mac の開発環境セットアップスクリプト
+# リラン可
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install tmux
+brew install tmux
+
+# install Emacs (with cocoa)
+brew cask install emacs
+
+# settings
+install ./zshrc ~/
+install ./tmux/2.9/.tmux.conf ~/
+install ./vimrc ~/
+
+# NeoBundle (for Vim) install
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
